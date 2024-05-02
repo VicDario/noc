7e0ab48c79fe6d8806f56fda043f731fe1ce85ad
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-01 21:12:07 -0400

    feat: add logger to check service

--- src/domain/use-cases/checks/check.service.ts
+++ src/domain/use-cases/checks/check.service.ts
@@ -1,25 +1,35 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entitity';
+import { LogRepository } from '../../repositories/log.repository';
+
 interface CheckServiceUseCase {
-    execute (url: string): Promise<boolean>
+  execute(url: string): Promise<boolean>;
 }
 
 type SuccessCallback = () => void | undefined;
 type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
-    constructor(
-        private readonly successCallback: SuccessCallback,
-        private readonly errorCallback: ErrorCallback
-    ) {}
+  constructor(
+    private readonly logRepository: LogRepository,
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
 
-    async execute(url: string): Promise<boolean> {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) throw new Error(`Error on check service ${url}`);
-            this.successCallback();
-            return response.ok;
-        } catch (error) {
-            this.errorCallback(`${error}`);
-            return false;
-        }
+  async execute(url: string): Promise<boolean> {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error(`Error on check service ${url}`);
+
+      const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
+      this.successCallback();
+      return response.ok;
+    } catch (error) {
+      const errorMessage = `${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
+      this.errorCallback(errorMessage);
+      return false;
     }
+  }
 }
