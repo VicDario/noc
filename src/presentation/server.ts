51f9b77c54d8f6e36ec65eb80bf194212f431f43
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-01 21:44:38 -0400

    feat: create instance for repository for using check service correctly

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,17 +1,20 @@
-import { CheckService } from "../domain/use-cases/checks/check.service";
-import { CronService } from "./cron/cron.service";
+import { CheckService } from '../domain/use-cases/checks/check.service';
+import { FileSystemDataSource } from '../infrastucture/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastucture/repositories/log.repository';
+import { CronService } from './cron/cron.service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
 
 export class Server {
-    public static start() {
-        console.log('Server started');
-        CronService.createJob(
-            '*/5 * * * * *',
-            () => {
-                new CheckService(
-                    () => console.log('success'),
-                    (error) => console.error(error)
-                ).execute('http://localhost:3000');
-            }
-        );
-    }
+  public static start() {
+    console.log('Server started');
+    const url = 'http://localhost';
+    CronService.createJob('*/5 * * * * *', () => {
+      new CheckService(
+        fileSystemLogRepository,
+        () => console.log('success'),
+        (error) => console.error(error)
+      ).execute(url);
+    });
+  }
 }
