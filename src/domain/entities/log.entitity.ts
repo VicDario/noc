800d7ddb54e3f2a71a7b9bf671833bace27b1520
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-01 20:47:22 -0400

    feat: add factory function for log entity

--- src/domain/entities/log.entitity.ts
+++ src/domain/entities/log.entitity.ts
@@ -14,4 +14,14 @@ export class LogEntity {
         this.level = level;
         this.createAt = new Date();
     }
+
+    // Factory function
+    static fromJson(json: string): LogEntity {
+        const { message, level, createAt} = JSON.parse(json);
+
+        const log = new LogEntity(message, level);
+        log.createAt = new Date(createAt);
+        
+        return log;
+    }
 }
