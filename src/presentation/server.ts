8a3e9a459af9126bc7d043af8d5e5232ba9ba4e3
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-13 19:17:57 -0400

    feat: use send mail

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,13 +1,25 @@
 import { CheckService } from '../domain/use-cases/checks/check.service';
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDataSource } from '../infrastucture/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastucture/repositories/log.repository';
 import { CronService } from './cron/cron.service';
+import { EmailService } from './email/email.service';
 
-const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource()
+);
+const sendEmailLogs = new SendEmailLogs(
+  new EmailService(),
+  fileSystemLogRepository
+);
 
 export class Server {
   public static start() {
     console.log('Server started');
+
+    /* sendEmailLogs
+      .execute('dariocontrerasfc@gmail.com'); */
+
     const url = 'http://localhost';
     CronService.createJob('*/5 * * * * *', () => {
       new CheckService(
