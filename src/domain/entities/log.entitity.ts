4c9c13834f64029f5bc21f149df7a154dd1d133b
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-13 17:36:22 -0400

    refactor: log entity now accept origin

--- src/domain/entities/log.entitity.ts
+++ src/domain/entities/log.entitity.ts
@@ -4,23 +4,32 @@ export enum LogSeverityLevel {
     high = 'high',
 }
 
+export interface LogEntityOptions {
+    message: string;
+    level: LogSeverityLevel;
+    origin: string;
+    createdAt?: Date;
+}
+
 export class LogEntity {
     public level: LogSeverityLevel;
     public message: string;
-    public createAt: Date;
+    public createdAt: Date;
+    public origin: string;
 
-    constructor(message: string, level: LogSeverityLevel) {
+    constructor(options: LogEntityOptions) {
+        const { message, level, origin, createdAt = new Date() } = options;
         this.message = message;
         this.level = level;
-        this.createAt = new Date();
+        this.origin = origin;
+        this.createdAt = createdAt;
     }
 
     // Factory function
     static fromJson(json: string): LogEntity {
-        const { message, level, createAt} = JSON.parse(json);
+        const { message, level, createdAt } = JSON.parse(json);
 
-        const log = new LogEntity(message, level);
-        log.createAt = new Date(createAt);
+        const log = new LogEntity({ message, level, origin, createdAt });
         
         return log;
     }
