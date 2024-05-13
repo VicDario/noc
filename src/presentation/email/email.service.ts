5886301f3780dfb7fa2361a1b16d35c8c2963101
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-13 18:44:45 -0400

    feat: add attachments and send logs

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -2,36 +2,68 @@ import { createTransport } from 'nodemailer';
 import { envs } from '../../config/plugins/env.plugin';
 
 interface SendMailOptions {
-    to: string;
-    subject: string;
-    htmlBody: string;
-    // TODO; attachments
+  to: string | string[];
+  subject: string;
+  htmlBody: string;
+  attachments?: Attachment[];
+}
+
+interface Attachment {
+  filename: string;
+  path: string;
 }
 
 export class EmailService {
-    private transporter = createTransport({
-        service: envs.MAILER_SERVICE,
-        auth: {
-            user: envs.MAILER_EMAIL,
-            pass: envs.MAILER_SECRET_KEY,
-        },
-    })
-    
-    constructor() { }
+  private transporter = createTransport({
+    service: envs.MAILER_SERVICE,
+    auth: {
+      user: envs.MAILER_EMAIL,
+      pass: envs.MAILER_SECRET_KEY,
+    },
+  });
 
-    async sendEmail(options: SendMailOptions): Promise<boolean> {
-        const { to, subject, htmlBody } = options;
-        try {
-            const sendInformation = await this.transporter.sendMail({
-                to,
-                subject,
-                html: htmlBody,
-            });
-            console.log(sendInformation);
-            
-            return true;
-        } catch (error) {
-            return false;
-        }
+  constructor() {}
+
+  async sendEmail(options: SendMailOptions): Promise<boolean> {
+    const { to, subject, htmlBody, attachments = [] } = options;
+    try {
+      const sendInformation = await this.transporter.sendMail({
+        to,
+        subject,
+        html: htmlBody,
+        attachments,
+      });
+
+      console.log(sendInformation);
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
     }
+  }
+
+  async sendEmailWithFileSystemLogs(to: string | string[]) {
+    const subject = 'Logs del servidor';
+    const htmlBody = `
+        <h3>Logs de sistema - NOC</h3>
+        <p>Lorem Ipsum</p>
+        `;
+    const attachments: Attachment[] = [
+      {
+        filename: 'logs-all.log',
+        path: './logs/logs-all.log',
+      },
+      {
+        filename: 'logs-high.log',
+        path: './logs/logs-high.log',
+      },
+      {
+        filename: 'logs-medium.log',
+        path: './logs/logs-medium.log',
+      },
+    ];
+
+    return await this.sendEmail({ to, subject, htmlBody, attachments });
+  }
 }
