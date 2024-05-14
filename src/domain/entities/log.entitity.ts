61dac4ec1fa85be86eb286201d2a481cb1604289
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-14 17:45:15 -0400

    feat: add new factory function from object

--- src/domain/entities/log.entitity.ts
+++ src/domain/entities/log.entitity.ts
@@ -33,4 +33,12 @@ export class LogEntity {
         
         return log;
     }
+
+    static fromObject(object: { [key: string]: any }): LogEntity {
+      const { message, level, origin, createdAt } = object;
+      
+      const log = new LogEntity({ message, level, origin, createdAt });
+        
+      return log;
+    }
 }
