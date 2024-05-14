8e2eb90011256f33a1c046269411010f95571118
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-14 17:50:30 -0400

    style: format

--- src/domain/entities/log.entitity.ts
+++ src/domain/entities/log.entitity.ts
@@ -1,44 +1,44 @@
 export enum LogSeverityLevel {
-    low = 'low',
-    medium = 'medium',
-    high = 'high',
+  low = 'low',
+  medium = 'medium',
+  high = 'high',
 }
 
 export interface LogEntityOptions {
-    message: string;
-    level: LogSeverityLevel;
-    origin: string;
-    createdAt?: Date;
+  message: string;
+  level: LogSeverityLevel;
+  origin: string;
+  createdAt?: Date;
 }
 
 export class LogEntity {
-    public level: LogSeverityLevel;
-    public message: string;
-    public createdAt: Date;
-    public origin: string;
+  public level: LogSeverityLevel;
+  public message: string;
+  public createdAt: Date;
+  public origin: string;
 
-    constructor(options: LogEntityOptions) {
-        const { message, level, origin, createdAt = new Date() } = options;
-        this.message = message;
-        this.level = level;
-        this.origin = origin;
-        this.createdAt = createdAt;
-    }
+  constructor(options: LogEntityOptions) {
+    const { message, level, origin, createdAt = new Date() } = options;
+    this.message = message;
+    this.level = level;
+    this.origin = origin;
+    this.createdAt = createdAt;
+  }
 
-    // Factory function
-    static fromJson(json: string): LogEntity {
-        const { message, level, createdAt } = JSON.parse(json);
+  // Factory function
+  static fromJson(json: string): LogEntity {
+    const { message, level, createdAt } = JSON.parse(json);
 
-        const log = new LogEntity({ message, level, origin, createdAt });
-        
-        return log;
-    }
+    const log = new LogEntity({ message, level, origin, createdAt });
 
-    static fromObject(object: { [key: string]: any }): LogEntity {
-      const { message, level, origin, createdAt } = object;
-      
-      const log = new LogEntity({ message, level, origin, createdAt });
-        
-      return log;
-    }
+    return log;
+  }
+
+  static fromObject(object: { [key: string]: any }): LogEntity {
+    const { message, level, origin, createdAt } = object;
+
+    const log = new LogEntity({ message, level, origin, createdAt });
+
+    return log;
+  }
 }
