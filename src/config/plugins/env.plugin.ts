16873dbee2eb0e019414b9367d149ddaa77f492f
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-14 17:44:49 -0400

    feat: add new env vars

--- src/config/plugins/env.plugin.ts
+++ src/config/plugins/env.plugin.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import * as env from 'env-var';
 
 export const envs = {
-    PORT: env.get('PORT').required().asPortNumber(),
-    MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
-    MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
-    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
-    PROD: env.get('PROD').required().asBool(),
+  PORT: env.get('PORT').required().asPortNumber(),
+  PROD: env.get('PROD').required().asBool(),
+  MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
+  MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
+  MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+  MONGO_URL: env.get('MONGO_URL').required().asUrlString(),
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
 }
