1d8b0b0a80e1c12512adc2bd7fb1b0b125655850
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-14 20:50:08 -0400

    feat: use postgres

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,24 +1,27 @@
-import { envs } from '../config/plugins/env.plugin';
+//import { envs } from '../config/plugins/env.plugin';
 import { CheckService } from '../domain/use-cases/checks/check.service';
-import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
+//import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 //import { FileSystemDataSource } from '../infrastucture/datasources/file-system.datasource';
-import { MongoLogDataSource } from '../infrastucture/datasources/mongo-log.datasource';
+//import { MongoLogDataSource } from '../infrastucture/datasources/mongo-log.datasource';
+import { PostgresLogDataSource } from '../data/postgres/init';
 import { LogRepositoryImpl } from '../infrastucture/repositories/log.repository';
 import { CronService } from './cron/cron.service';
-import { EmailService } from './email/email.service';
+//import { EmailService } from './email/email.service';
 
 const LogRepository = new LogRepositoryImpl(
   //new FileSystemDataSource()
-  new MongoLogDataSource()
+  //new MongoLogDataSource()
+  new PostgresLogDataSource()
 );
-const sendEmailLogs = new SendEmailLogs(
+
+/* const sendEmailLogs = new SendEmailLogs(
   new EmailService({
     service: envs.MAILER_SERVICE,
     mailerEmail: envs.MAILER_EMAIL,
     mailerSecretKey: envs.MAILER_SECRET_KEY,
   }),
   LogRepository
-);
+); */
 
 export class Server {
   public static start() {
@@ -27,7 +30,7 @@ export class Server {
     /* sendEmailLogs
       .execute('dariocontrerasfc@gmail.com'); */
 
-    const url = 'https://google.com';
+    const url = 'https://gsadsadvdcxsd.com';
     CronService.createJob('*/5 * * * * *', () => {
       new CheckService(
         LogRepository,
