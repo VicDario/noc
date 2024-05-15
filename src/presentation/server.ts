e7fd1a2380ba272b6b7f173d5c6c365142390175
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-14 21:05:03 -0400

    feat: use check multiple service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,42 +1,26 @@
-//import { envs } from '../config/plugins/env.plugin';
-import { CheckService } from '../domain/use-cases/checks/check.service';
-//import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
-//import { FileSystemDataSource } from '../infrastucture/datasources/file-system.datasource';
-//import { MongoLogDataSource } from '../infrastucture/datasources/mongo-log.datasource';
+import { CheckMultipleService } from '../domain/use-cases/checks/check-multiple.service';
+import { FileSystemDataSource } from '../infrastucture/datasources/file-system.datasource';
+import { MongoLogDataSource } from '../infrastucture/datasources/mongo-log.datasource';
 import { PostgresLogDataSource } from '../data/postgres/init';
 import { LogRepositoryImpl } from '../infrastucture/repositories/log.repository';
 import { CronService } from './cron/cron.service';
-//import { EmailService } from './email/email.service';
 
-const LogRepository = new LogRepositoryImpl(
-  //new FileSystemDataSource()
-  //new MongoLogDataSource()
-  new PostgresLogDataSource()
-);
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDataSource());
+const postgreLogRepository = new LogRepositoryImpl(new PostgresLogDataSource());
 
-/* const sendEmailLogs = new SendEmailLogs(
-  new EmailService({
-    service: envs.MAILER_SERVICE,
-    mailerEmail: envs.MAILER_EMAIL,
-    mailerSecretKey: envs.MAILER_SECRET_KEY,
-  }),
-  LogRepository
-); */
 
 export class Server {
   public static start() {
     console.log('Server started');
 
-    /* sendEmailLogs
-      .execute('dariocontrerasfc@gmail.com'); */
-
     const url = 'https://gsadsadvdcxsd.com';
     CronService.createJob('*/5 * * * * *', () => {
-      new CheckService(
-        LogRepository,
-        () => console.log('success'),
-        (error) => console.error(error)
-      ).execute(url);
+      new CheckMultipleService([
+        fsLogRepository,
+        mongoLogRepository,
+        postgreLogRepository,
+      ]).execute(url);
     });
   }
 }
